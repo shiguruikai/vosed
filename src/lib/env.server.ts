e0bce2a8b1ab55d90c307e8798c1bfda1b235e09
shiguruikai <kai.shigurui@gmail.com>
2026-06-19 00:24:42 +0900

    fix: OUTPUT_DIRのデフォルト値を修正

--- src/lib/env.server.ts
+++ src/lib/env.server.ts
@@ -5,7 +5,7 @@ import path from 'node:path';
 export function getSettings() {
   const ttsApiBaseUrl = process.env.TTS_API_BASE_URL || 'http://localhost:8088';
 
-  let outputDir = path.resolve(process.env.OUTPUT_DIR || path.join(process.cwd(), 'output'));
+  let outputDir = path.resolve(process.env.OUTPUT_DIR || './output');
   // 末尾に必ず区切り文字を付ける。
   // ⇒ これにより、絶対パスの前方一致チェックでディレクトリトラバーサル対策ができる。
   if (!outputDir.endsWith(path.sep)) {
